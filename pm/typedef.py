81f6ef3e14ef15518d6356942e12e67fb85dfde0
stanislavsabev <bezraboten.34@gmail.com>
2023-12-04 14:53:50 +0100

    Remove typer  and typing-extensions from the requirements.

--- pm/typedef.py
+++ pm/typedef.py
@@ -1,17 +1,9 @@
 """Type definitions for the project."""
 import typing
 
-import typer
-import typing_extensions
-
-Annotated = typing_extensions.Annotated
-
 AnyDict = dict[str, typing.Any]
 StrDict = dict[str, str]
 LStr = list[str]
 LStrDict = dict[str, list[str]]
-
-ProjOpt = Annotated[typing.Optional[str], typer.Argument(help="Project name")]
-WtOpt = Annotated[typing.Optional[str], typer.Argument(help="Worktree name")]
 
 RecordTuple = tuple[str, str | None, str | None]
