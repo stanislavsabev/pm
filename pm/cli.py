56d04fe2c4a12c9eac74e3188d10443e513eca2e
stanislavsabev <bezraboten.34@gmail.com>
2024-01-12 15:24:53 +0100

    Add logging

--- pm/cli.py
+++ pm/cli.py
@@ -1,16 +1,20 @@
 """Handler for calling the `pm` package directly from the command line."""
+import logging
 import sys
+import traceback
 
 from pm import argparse
 from pm import commands
 from pm import const
+
+logger = logging.getLogger("pm")
 
 
 def die(msg: str | None) -> None:
     """Print message end exit with error."""
     if msg:
         print(f"{const.APP_NAME}: {msg}")
-        sys.exit(1)
+    sys.exit(1)
 
 
 def app() -> None:
@@ -18,9 +22,10 @@ def app() -> None:
     try:
         args: commands.AppArgs = argparse.parse()
         if args.command:
+            logger.debug(f"Running command {args.command}")
             args.command.run(args)
-    except Exception as e:
-        die("\n".join(e.args))
+    except Exception:
+        die(traceback.format_exc())
 
 
 if __name__ == "__main__":
